5c8ad01520ead1b0aa367eb616111a1caf987d2d
Diego Quintero <daquinterop@gmail.com>
2020-06-01 18:56:13 -0500

    Añadido el esqueleto de un aplicación dash

---
+++ apps/app2.py
@@ -0,0 +1,26 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+
+layout = html.Div([
+    html.H2('Page 2'),
+    dcc.Dropdown(
+        id='page-2-dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='page-2-display-value'),
+    html.Br(),
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-1"', href='/page-1'),
+])
+
+# Page 2 callbacks
+@app.callback(Output('page-2-display-value', 'children'),
+              [Input('page-2-dropdown', 'value')])
+def display_value(value):
+    print('display_value')
+    return 'You have selected "{}"'.format(value)
