1bd27c744d2dbe5ece26849c69c804677b2fd52a
Diego Quintero <daquinterop@gmail.com>
2020-07-17 15:45:53 -0500

    Incluida carpeta para plots json

--- graphs.py
+++ graphs.py
@@ -1,10 +1,17 @@
 import plotly.express as px
 import datasets as data
 import dash_bootstrap_components as dbc
+import json
+import plotly.io as pio
 
 def test_graph(iris):
     fig = px.scatter(iris, x="sepal_width", y="sepal_length", color="species",
                  size='petal_length', hover_data=['petal_width'])
     return fig
+
+def load_json_figure(json_path):
+    with open(json_path, 'r') as f:
+        json_figure = json.load(f)
+    return pio.from_json(json.dumps(json_figure))
 
 table = dbc.Table.from_dataframe(data.df, striped=True, bordered=True, hover=True)
